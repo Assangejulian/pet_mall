681ce66e8b89c9510252f0fb1a44323294479664
moderlick <15855837+moderlick@user.noreply.gitee.com>
2026-06-30 03:33:53 -0400

    fix(cart): persist cart through backend flow

--- pet_miniapp/app.js
+++ pet_miniapp/app.js
@@ -2,15 +2,12 @@ App({
   globalData: {
     baseUrl: "http://192.168.123.234:8080",
     // baseUrl: "http://127.0.0.1:8080",
-    cart: [],
     user: null,
     token: ""
   },
 
   onLaunch() {
-    const cart = wx.getStorageSync("cart") || [];
     const token = wx.getStorageSync("token") || "";
-    this.globalData.cart = cart;
     this.globalData.token = token;
   },
 
@@ -19,33 +16,5 @@ App({
     if (this.globalData.token) return true;
     wx.navigateTo({ url: "/subpages/login/login" });
     return false;
-  },
-
-  getCartCount() {
-    return this.globalData.cart.reduce((sum, item) => sum + item.quantity, 0);
-  },
-
-  addToCart(product) {
-    const cart = this.globalData.cart;
-    const index = cart.findIndex((item) => item.id === product.id);
-    if (index > -1) {
-      cart[index].quantity += 1;
-    } else {
-      cart.push({
-        id: product.id,
-        name: product.name,
-        price: product.price,
-        image: product.image,
-        quantity: 1,
-        checked: true
-      });
-    }
-    this.globalData.cart = cart;
-    wx.setStorageSync("cart", cart);
-  },
-
-  updateCart(cart) {
-    this.globalData.cart = cart;
-    wx.setStorageSync("cart", cart);
   }
 });
